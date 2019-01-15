190b7f5ebad4adde6b847d5b063efd9b4451ffcf
Lucas Kauff <l.kauff@rubika-edu.com>
2019-01-16 00:08:24 +0100

    Developing the dialogue system
    Bug: second hieroglyph misses a sentence

--- PitstopTest0/Assets/Scripts/Hieroglyphs_Activator.cs
+++ PitstopTest0/Assets/Scripts/Hieroglyphs_Activator.cs
@@ -2,29 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hieroglyphs_Activator1 : MonoBehaviour
+public class Hieroglyphs_Activator : MonoBehaviour
 {
     [SerializeField]
     DialogueManager diaMan;
     [SerializeField]
     DialogueTrigger diaTrig;
 
-    bool playerRead = false;
+    bool playerReading = false;
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if (playerReading && Input.GetKeyDown(KeyCode.E))
         {
             diaMan.DisplayNextSentence();
         }
     }
 
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(playerRead == false)
+        if(playerReading == false)
         {
-            playerRead = true;
+            playerReading = true;
             diaTrig.TriggerDialogue();
-        }        
+        }
     }
 }
