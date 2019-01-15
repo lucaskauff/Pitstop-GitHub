b70721d161b63c2cf9ffe7d0c789a2f0abd4df46
Lucas Kauff <l.kauff@rubika-edu.com>
2019-01-15 22:15:55 +0100

    Developing the dialogue system
    first hieroglyphs

---
+++ PitstopTest0/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject nameText;
+    public GameObject dialogueText;
+
+    Queue<string> sentences;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        nameText.GetComponent<TextMeshProUGUI>().text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+
+        //Displays the whole sentence
+        //dialogueText.GetComponent<TextMeshProUGUI>().text = sentence;
+
+        //Displays letter by letter
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.GetComponent<TextMeshProUGUI>().text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.GetComponent<TextMeshProUGUI>().text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+
+    }
+}
---
+++ PitstopTest0/Assets/Scripts/Hieroglyphs_Activator1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hieroglyphs_Activator1 : MonoBehaviour
+{
+    [SerializeField]
+    DialogueManager diaMan;
+    [SerializeField]
+    DialogueTrigger diaTrig;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            diaMan.DisplayNextSentence();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        diaTrig.TriggerDialogue();
+    }
+}
