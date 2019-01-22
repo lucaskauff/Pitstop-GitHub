5882a18fe6e2177f02cc960fec4cf1fd77cf6b49
Lucas Kauff <l.kauff@rubika-edu.com>
2019-01-22 01:06:58 +0100

    BearFight scene added
    + first dialogue wheel tests

---
+++ PitstopTest0/Assets/Scripts/DialogueWheelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueWheelManager : MonoBehaviour
+{
+    public GameObject[] dialogueWheelSlotsV1;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            for (int i = 0; i < dialogueWheelSlotsV1.Length; i++)
+            {
+                dialogueWheelSlotsV1[i].SetActive(true);
+            }            
+        }
+        else
+        {
+            for (int i = 0; i < dialogueWheelSlotsV1.Length; i++)
+            {
+                dialogueWheelSlotsV1[i].SetActive(false);
+            }
+        }
+    }
+}
