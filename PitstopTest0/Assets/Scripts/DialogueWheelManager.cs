4f7b6752322c6dff916f7c51ab66a941262b6294
Lucas Kauff <l.kauff@rubika-edu.com>
2019-01-22 19:14:30 +0100

    Dialogue Wheel dev
    Panels are clickable

--- PitstopTest0/Assets/Scripts/DialogueWheelManager.cs
+++ PitstopTest0/Assets/Scripts/DialogueWheelManager.cs
@@ -8,6 +8,13 @@ public class DialogueWheelManager : MonoBehaviour
 
     private void Update()
     {
+        Vector2 playerPos = transform.position;
+        Vector2 wheelDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Vector2 wheelOrigin = playerPos + wheelDirection.normalized;
+
+        RaycastHit2D hit = Physics2D.Raycast(wheelOrigin, wheelDirection, 10);
+        Debug.DrawRay(wheelOrigin, wheelDirection, Color.red);
+        
         if (Input.GetKey(KeyCode.Space))
         {
             for (int i = 0; i < dialogueWheelSlotsV1.Length; i++)
