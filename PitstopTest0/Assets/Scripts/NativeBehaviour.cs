59cc406dd6c94ac657cb1a74b705ebb9f0a7dec2
Lucas Kauff <l.kauff@rubika-edu.com>
2019-01-25 00:32:16 +0100

    Native throws spear !
    East and West for now

--- PitstopTest0/Assets/Scripts/NativeBehaviour.cs
+++ PitstopTest0/Assets/Scripts/NativeBehaviour.cs
@@ -6,9 +6,19 @@ public class NativeBehaviour : MonoBehaviour
 {
     public string whereToShoot;
 
-    public Transform spearSpawner;
+    public Transform[] spearSpawners;
     public GameObject spear;
+
+    [SerializeField]
+    float throwRange;
+
+    Vector2 nativePos;
     GameObject cloneSpear;
+
+    void Start()
+    {
+        nativePos = transform.position;
+    }
 
     void Update()
     {
@@ -20,11 +30,21 @@ public class NativeBehaviour : MonoBehaviour
 
     void Shot()
     {
-        
-
         if (whereToShoot == "SlotE")
         {
-            cloneSpear = (GameObject)Instantiate(spear, spearSpawner.position, spear.transform.rotation);
+            cloneSpear = (GameObject)Instantiate(spear, spearSpawners[1].position, spear.transform.rotation);
+            cloneSpear.GetComponent<SpearBehaviour>().targetPos = new Vector2(nativePos.x + throwRange, nativePos.y);
+            cloneSpear.GetComponent<SpearBehaviour>().isFired = true;
+            whereToShoot = null;
+        }
+
+        if (whereToShoot == "SlotW")
+        {
+            cloneSpear = (GameObject)Instantiate(spear, spearSpawners[3].position, spear.transform.rotation);
+            cloneSpear.transform.Rotate(new Vector3(0, -180, 0));
+            cloneSpear.GetComponent<SpearBehaviour>().targetPos = new Vector2(nativePos.x - throwRange, nativePos.y);
+            cloneSpear.GetComponent<SpearBehaviour>().isFired = true;
+            whereToShoot = null;
         }
     }
 }
