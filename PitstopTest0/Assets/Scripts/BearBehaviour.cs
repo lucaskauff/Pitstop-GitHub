c23e802865e63acc28ad11d8f3932ec5f83813a0
Lucas Kauff <l.kauff@rubika-edu.com>
2019-01-25 01:08:19 +0100

    Bear integrated

---
+++ PitstopTest0/Assets/Scripts/BearBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    float lineOfSightRad = 2.5f;
+
+    Rigidbody2D myRb;
+    CircleCollider2D lineOfSight;
+
+    private void Start()
+    {
+        myRb = GetComponent<Rigidbody2D>();
+        lineOfSight = GetComponent<CircleCollider2D>();
+        lineOfSight.radius = lineOfSightRad;
+    }
+}
