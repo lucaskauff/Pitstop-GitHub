37574d7fc2aba2c0cb5dbdb2ee24bb3a37537e6e
Luc <ldrn38@orange.fr>
2019-02-05 18:32:40 +0100

    Prototype Dialogue Wheel #1

---
+++ PitstopTest0/Assets/Scripts/Dialogue/LUD_TestDialogue/LUD_DialogueManagement.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LUD_DialogueManagement : MonoBehaviour
+{
+    //SerializeField
+    [SerializeField]
+    GameObject dialogueWheel;
+
+    [Header("Sentence Parameters")]
+
+    [SerializeField]
+    private List<int> sentence = new List<int>();
+
+    [Header("Natives Parameters")]
+
+    [SerializeField]
+    GameObject[] nativesList;
+
+    
+
+    //Private
+
+
+    
+    void Start()
+    {
+        
+        sentence.Clear();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            WheelAppearance();
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            WheelDisappearance();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            DeleteLastWord();
+        }
+
+    }
+
+
+    void WheelAppearance()
+    {
+        dialogueWheel.SetActive(true);
+    }
+    
+    void WheelDisappearance()
+    {
+        dialogueWheel.SetActive(false);
+
+        SentenceIsPrononced();
+
+
+    }
+
+    public void AddAWordToTheSentence (int valueOfTheWord)
+    {
+        if (sentence.Count <3)
+        {
+            sentence.Add(valueOfTheWord);
+            //Debug.Log("word = " + valueOfTheWord.ToString());
+        }
+
+    }
+
+    public void DeleteLastWord ()
+    {
+        if (sentence.Count > 0)
+        {
+            sentence.RemoveAt(sentence.Count - 1);
+            //Debug.Log("Word remove");
+        }
+
+        
+    }
+
+    void SentenceIsPrononced()
+    {
+        if (sentence.Count != 0)
+        {
+            nativesList[0].GetComponent<LUD_NativeHeartheSentence>().HearASentence(sentence);
+        }
+        
+
+        sentence.Clear();
+        //Debug.Log("Sentence Clear");
+    }
+
+}
---
+++ PitstopTest0/Assets/Scripts/Dialogue/LUD_TestDialogue/LUD_NativeHeartheSentence.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LUD_NativeHeartheSentence : MonoBehaviour
+{
+
+    //SerializeField
+    [SerializeField]
+    bool isCaptivated = true;
+
+
+    //Private
+
+    //Public
+
+    public void HearASentence(List<int> heardSentence)
+    {
+        if (isCaptivated)
+        {
+            int sumOfSentence = ValueOfTheSentence(heardSentence);
+
+            
+
+            if (sumOfSentence == 3)
+            {
+                NativeAnswer("BETTERAVE !!");
+            }
+
+            else if (sumOfSentence == 9 || sumOfSentence == 27)
+            {
+                NativeAnswer("POMME !!");
+            }
+
+            else if (sumOfSentence == 15 || sumOfSentence == 11)
+            {
+                NativeAnswer("CHAMPIGNON !!");
+            }
+
+            else
+            {
+                NativeAnswer("Il n'y a rien !");
+            }
+        }
+        
+    }
+
+    void NativeAnswer(string answer)
+    {
+        Debug.Log("Le natif répond : " + answer);
+    }
+
+    int ValueOfTheSentence (List<int> listOfInt)
+    {
+        int sum = 1;
+
+        foreach (int item in listOfInt)
+        {
+            sum = sum * item;
+        }
+
+        Debug.Log("sum = " + sum.ToString());
+
+        return sum;
+    }
+
+}
