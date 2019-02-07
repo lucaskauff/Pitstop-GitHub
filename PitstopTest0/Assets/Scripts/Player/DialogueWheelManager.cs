3d41a094b30f55237ab2d076fa80a17aa0a53a58
Lucas Kauff <l.kauff@rubika-edu.com>
2019-02-07 15:55:38 +0100

    Feierabend

--- PitstopTest0/Assets/Scripts/Player/DialogueWheelManager.cs
+++ PitstopTest0/Assets/Scripts/Player/DialogueWheelManager.cs
@@ -5,18 +5,9 @@ using UnityEngine;
 public class DialogueWheelManager : MonoBehaviour
 {
     public GameObject[] dialogueWheelSlotsV1;
-    //public LayerMask layerRaycast;
 
     private void Update()
     {
-        /*
-        Vector2 playerPos = transform.position;
-        Vector2 cursorDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-
-        RaycastHit2D hit = Physics2D.Raycast(playerPos, cursorDirection, 10, layerRaycast);
-        Debug.DrawRay(playerPos, cursorDirection, Color.red);
-        */
-
         //Show dialogue wheel slots on space maintained pressed
         if (Input.GetKey(KeyCode.Space))
         {
