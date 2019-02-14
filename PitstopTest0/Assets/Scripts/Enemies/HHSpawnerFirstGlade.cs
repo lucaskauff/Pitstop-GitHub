63571e77e11effc1a989406d6d3806016b297dff
Lucas Kauff <l.kauff@rubika-edu.com>
2019-02-14 18:35:50 +0100

    Forest Scene
    Start of the cinemachine

---
+++ PitstopTest0/Assets/Scripts/Enemies/HHSpawnerFirstGlade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HHSpawnerFirstGlade : MonoBehaviour
+{
+    public bool spawnTheThing = true;
+    public GameObject whatToSpawn;
+    GameObject theSpawnedThing;
+    bool spawnCheck = false;
+
+    private void Start()
+    {
+        whatToSpawn = GameObject.Find("FirstHH");
+    }
+
+    private void Update()
+    {
+        if (spawnTheThing && !spawnCheck)
+        {
+            theSpawnedThing = (GameObject)Instantiate(whatToSpawn, transform.position, whatToSpawn.transform.rotation);
+
+        }
+    }
+}
---
+++ PitstopTest0/Assets/Scripts/Glade1Cinematic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Glade1Cinematic : MonoBehaviour
+{
+    public bool startCinematic = false;
+    public GameObject virtualCameraPlayer;
+    public Transform centerOfTheGlade;
+    public HHSpawnerFirstGlade spawner;
+
+    private void Update()
+    {
+        if (startCinematic == true)
+        {
+            virtualCameraPlayer.SetActive(false);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject.name == "Zayn")
+        {
+            startCinematic = true;
+        }
+    }
+}
