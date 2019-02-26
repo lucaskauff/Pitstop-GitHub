b950e20da2b1df152ee6b49a1324ff73b47e8432
Lucas Kauff <l.kauff@rubika-edu.com>
2019-02-26 20:47:40 +0100

    SandboxScene + FOREST

---
+++ PitstopTest0/Assets/Scripts/Enemies/HHRange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HHRange : MonoBehaviour
+{
+    public HHBehaviour hammerheadBeh;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        //condition not perfect !!!
+        if (!hammerheadBeh.isArrived && hammerheadBeh.target == null && (collider.gameObject.name == "Zayn" || collider.gameObject.name == "Native"))
+        {
+            hammerheadBeh.target = collider.gameObject;
+        }
+    }
+}
