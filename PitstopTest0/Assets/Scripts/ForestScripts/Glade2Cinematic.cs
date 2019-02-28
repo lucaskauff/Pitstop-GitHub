f4fa438096e32b6b391f64813e1fd2169e5dc51c
Lucas Kauff <l.kauff@rubika-edu.com>
2019-02-28 10:13:10 +0100

    FOREST
    Cinematics and holes done !

---
+++ PitstopTest0/Assets/Scripts/ForestScripts/Glade2Cinematic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glade2Cinematic : MonoBehaviour
+{
+    public GameObject virtualCameraPlayer;
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject.name == "Zayn")
+        {
+            virtualCameraPlayer.SetActive(false);
+        }
+    }
+}
