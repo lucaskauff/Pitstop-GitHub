54d4c9a620fb35af61d114652bc3cef6fd21a9bb
Lucas Kauff <l.kauff@rubika-edu.com>
2019-03-06 19:48:52 +0100

    TEMPLE
    Integration EasySave

--- PitstopTest0/Assets/Scripts/GridScripts/Stairs.cs
+++ PitstopTest0/Assets/Scripts/GridScripts/Stairs.cs
@@ -6,22 +6,27 @@ using Cinemachine;
 public class Stairs : MonoBehaviour
 {
     Collider2D myCollider;
-    public float stairsIsometricRatio = 0.8f;
+    [SerializeField]
+    CinemachineImpulseSource myImpulseSource;
+    [SerializeField]
+    float stairsIsometricRatio = 0.8f;
 
     private void Start()
     {
         myCollider = GetComponent<Collider2D>();
     }
 
+    //Sets up a new isometric ratio for the player + generates light impulse
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Zayn" && collision.gameObject.GetComponent<PlayerControllerIso>().isMoving)
         {
             collision.gameObject.GetComponent<PlayerControllerIso>().isometricRatio = stairsIsometricRatio;
-            collision.gameObject.GetComponent<CinemachineImpulseSource>().GenerateImpulse();
+            myImpulseSource.GenerateImpulse();
         }
     }
 
+    //Resets the player's regular isometric ratio
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Zayn")
