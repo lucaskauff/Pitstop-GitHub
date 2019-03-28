af48fd538793f031bf6afbbaa9c61a80912fbcdb
Lucas Kauff <l.kauff@rubika-edu.com>
2019-03-28 19:59:45 +0100

    FOREST Mock-up

--- PitstopTest0/Assets/1_Scripts/ForestScripts/Glade1Tree.cs
+++ PitstopTest0/Assets/1_Scripts/ForestScripts/Glade1Tree.cs
@@ -6,11 +6,24 @@ namespace Pitstop
 {
     public class Glade1Tree : MonoBehaviour
     {
-        [SerializeField] GameObject impulseApple;
+        [SerializeField] GameObject impulseApple = default;
+        [SerializeField] Transform appleImpactOnGround = default;
+        [SerializeField] float fallSpeed = 2;
+
+        private void OnColliderEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                SpawnAnApple();
+            }
+        }
 
         public void SpawnAnApple()
         {
-
+            impulseApple.GetComponent<ScannableObjectBehaviour>().targetPos = appleImpactOnGround.position;
+            impulseApple.GetComponent<ScannableObjectBehaviour>().projectileSpeed = fallSpeed;
+            impulseApple.GetComponent<ScannableObjectBehaviour>().isScannable = false;
+            impulseApple.GetComponent<ScannableObjectBehaviour>().isFired = true;
         }
     }
 }
