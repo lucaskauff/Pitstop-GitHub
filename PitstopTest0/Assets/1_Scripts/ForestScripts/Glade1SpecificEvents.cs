639d9874a4d6f5e2757a5dc5aeef43c58335fc56
Lucas Kauff <l.kauff@rubika-edu.com>
2019-04-11 21:42:09 +0200

    FOREST : Glade 1 - 50%

---
+++ PitstopTest0/Assets/1_Scripts/ForestScripts/Glade1SpecificEvents.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pitstop
+{
+    public class Glade1SpecificEvents : MonoBehaviour
+    {
+        [SerializeField] EnemySpawner hHNest = default;
+
+        private bool playerEnteredCheck = false;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                Debug.Log("Player entered Glade1.");
+                hHNest.SpawnTheThing();
+            }
+        }
+    }
+}
