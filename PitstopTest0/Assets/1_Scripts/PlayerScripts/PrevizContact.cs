80c4ce7787e826248a8ac025a0ac78829409dc2e
Lucas Kauff <l.kauff@rubika-edu.com>
2019-04-15 12:42:10 +0200

    Worked on develop; fail

--- PitstopTest0/Assets/1_Scripts/PlayerScripts/PrevizContact.cs
+++ PitstopTest0/Assets/1_Scripts/PlayerScripts/PrevizContact.cs
@@ -10,7 +10,7 @@ namespace Pitstop
 
         private void OnTriggerStay2D(Collider2D collision)
         {
-            if (collision.gameObject.tag == "ShootableArea" || collision.gameObject.tag == "PressurePlate")
+            if (collision.gameObject.tag == "ShootableArea" || collision.gameObject.tag == "PressurePlate" || collision.gameObject.tag == "Player")
             {
                 objectShootable = true;
             }
@@ -22,7 +22,7 @@ namespace Pitstop
 
         private void OnTriggerExit2D(Collider2D collision)
         {
-            if (collision.gameObject.tag == "ShootableArea" || collision.gameObject.tag == "PressurePlate")
+            if (collision.gameObject.tag == "ShootableArea" || collision.gameObject.tag == "PressurePlate" || collision.gameObject.tag == "Player")
             {
                 objectShootable = false;
             }
