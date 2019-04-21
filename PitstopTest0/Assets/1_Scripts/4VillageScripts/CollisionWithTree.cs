1e20e7404b74bcbd1f932da55edfe6c46350dd31
Lucas Kauff <l.kauff@rubika-edu.com>
2019-04-21 23:09:32 +0200

    VILLAGE : Tree Anims

---
+++ PitstopTest0/Assets/1_Scripts/4VillageScripts/CollisionWithTree.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionWithTree : MonoBehaviour
+{
+    Animator myAnim;
+
+    private void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy")
+        {
+            myAnim.SetTrigger("Collision");
+        }
+    }
+}
