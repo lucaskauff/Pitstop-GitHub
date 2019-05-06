034facbed3eeb8c1e1165cd6cbf1144dbe428fcf
l <l.derain@rubika-edu.com>
2019-05-06 20:17:36 +0200

    New PNJ + Integration Of Rebecca

---
+++ PitstopTest0/Assets/3_Sprites/UISprites/Dialogue Wheel Sprites/Rebecca test/LUD_SeeSpriteAnimation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Pitstop
+{
+
+    public class LUD_SeeSpriteAnimation : MonoBehaviour
+    {
+
+        public Sprite seeSprite1;
+        public Sprite seeSprite2;
+        public float timeBetweenTwoSprites = 1f;
+
+        bool isFirstFrameDisplay = true;
+
+        private void Start()
+        {
+            GetComponent<Image>().sprite = seeSprite1;
+            StartCoroutine(WaitBeforeChangeSprite());
+        }
+
+        IEnumerator WaitBeforeChangeSprite()
+        {
+            yield return new WaitForSeconds(timeBetweenTwoSprites);
+            
+            if (isFirstFrameDisplay)
+            {
+                GetComponent<Image>().sprite = seeSprite1;
+            }
+            else
+            {
+                GetComponent<Image>().sprite = seeSprite2;
+            }
+
+            isFirstFrameDisplay = !isFirstFrameDisplay;
+            
+            StartCoroutine(WaitBeforeChangeSprite());
+        }
+
+    }
+
+}
