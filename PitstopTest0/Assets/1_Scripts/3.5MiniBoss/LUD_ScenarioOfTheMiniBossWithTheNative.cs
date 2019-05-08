d879476e3609a283787d4c2f93305dbbf1b0be4c
l <l.derain@rubika-edu.com>
2019-05-08 23:33:18 +0200

    Beggining Of The Scenario Of The MiniBoss

---
+++ PitstopTest0/Assets/1_Scripts/3.5MiniBoss/LUD_ScenarioOfTheMiniBossWithTheNative.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pitstop
+{
+    public class LUD_ScenarioOfTheMiniBossWithTheNative : MonoBehaviour
+    {
+        /*
+        public PlayerControllerIso playerController;
+
+        [Header("Zayn movements")]
+        public Transform pointWhereZaynGoAtTheBeggining;
+        private bool isArrivedToFirstPoint = false;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+            //playerController.canMove = false;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+
+        }
+        */
+    }
+}
