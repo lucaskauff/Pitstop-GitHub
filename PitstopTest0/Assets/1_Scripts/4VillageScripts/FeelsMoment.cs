150a60f88ad7cf30e435a700067f6387b2aef87f
Lucas Kauff <l.kauff@rubika-edu.com>
2019-05-23 03:10:06 +0200

    I gotta a feeling

---
+++ PitstopTest0/Assets/1_Scripts/4VillageScripts/FeelsMoment.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pitstop
+{
+    public class FeelsMoment : MonoBehaviour
+    {
+        [SerializeField] float secondsToWait = 5;
+        [SerializeField] GameObject vCamHorizon = default;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                StartCoroutine(WaitForPlayerToStay());
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                StopAllCoroutines();
+                vCamHorizon.SetActive(false);
+            }
+        }
+
+        IEnumerator WaitForPlayerToStay()
+        {
+            yield return new WaitForSeconds(secondsToWait);
+            vCamHorizon.SetActive(true);
+        }
+    }
+}
