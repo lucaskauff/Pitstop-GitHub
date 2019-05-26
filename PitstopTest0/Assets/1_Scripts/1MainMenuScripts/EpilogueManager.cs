cbf8133d700ec9de6bcff756d36444b28042c402
Lucas Kauff <l.kauff@rubika-edu.com>
2019-05-27 01:52:56 +0200

    BUILD 1.1 DONE

---
+++ PitstopTest0/Assets/1_Scripts/1MainMenuScripts/EpilogueManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pitstop
+{
+    public class EpilogueManager : MonoBehaviour
+    {
+        SceneLoader sceneLoader;
+
+        [SerializeField] float epilogueTimer = 35;
+        [SerializeField] string nextScene = null;
+
+        private void Awake()
+        {
+            StartCoroutine(EpilogueIsPlaying());
+        }
+
+        private void Start()
+        {
+            sceneLoader = GameManager.Instance.sceneLoader;
+        }
+
+        IEnumerator EpilogueIsPlaying()
+        {
+            yield return new WaitForSeconds(epilogueTimer);
+            sceneLoader.LoadNewScene(nextScene);
+        }
+    }
+}
